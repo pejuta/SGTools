4de46bb1a871f56e705e42001db6a47a943b385a
pejuta <sp_dragon3125@hotmail.co.jp>
2021-06-19 20:02:10 +0900

    Update SGActionCharaSelector.user.js
    500エラーなどでスキル情報が読めなかった後のトグルの挙動を修正

--- UserScripts/SGActionCharaSelector.user.js
+++ UserScripts/SGActionCharaSelector.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://twitter.com/11powder
 // @description Stroll Greenの各種行動画面のキャラ選択を便利にする
 // @include     http://st.x0.to/?mode=keizoku4*
-// @version     1.0.3
+// @version     1.0.4
 // @updateURL   https://pejuta.github.io/SGTools/UserScripts/SGActionCharaSelector.user.js
 // @downloadURL https://pejuta.github.io/SGTools/UserScripts/SGActionCharaSelector.user.js
 // @grant       none
@@ -14,9 +14,11 @@ await (async () => {
 
     const _vdoc = document.implementation.createHTMLDocument();
     const $inputEnos = $("#d1,#d2,#d3");
+
+    // .charaframeselfに対してバインドしてはならない
     function toggleSelectedChara($target) {
         if (!$target.is(".charaframe,.charaframe2")) {
-            return false;
+            return "notcharaframe";
         }
 
         const targetEno = $target.data("eno") + "";
@@ -28,19 +30,21 @@ await (async () => {
         } else if ($firstEmptyInput.length > 0) {
             $firstEmptyInput.val(targetEno);
         } else {
-            return false;
+            return "noemptyslot";
         }
 
         $target.toggleClass("charaframe").toggleClass("charaframe2");
         $target.find(".tubuyaki").toggleClass("inline");
         $target.find(".pin").toggleClass("block");
 
-        return true;
+        return null;
     }
 
+    // toggleSelectedCharaとともに呼ぶ順序によって結果が変わるため要注意
     async function toggleSkillsOfSelectedChara($target) {
         if (!$target.is(".charaframe,.charaframe2")) {
-            return false;
+            // .charaframe2 は .charaframeself のブロックにも付与されている
+            return "notcharaframe";
         }
 
         const targetEno = $target.data("eno") + "";
@@ -48,20 +52,25 @@ await (async () => {
         const $skills = $target.children("." + skillsClassname);
         if ($skills.length > 0) {
             $skills.toggle();
-            return false;
+            return null;
+        }
+        else if ($target.is(".charaframe")) {
+            // キャラは選択されているがスキル情報は出ていない状態だった
+            // スキル情報ロード失敗時などの後にここに来るが、あくまで正常な処理の範疇
+            return null
         }
 
         const res = await fetch(`http://st.x0.to/?mode=profile&eno=${targetEno}`);
         if (!res.ok) {
             $target.append(`<div class="${skillsClassname + "err"}">スキル一覧の取得に失敗しました。</div>`);
             setTimeout(() => $target.find($("." + skillsClassname + "err")).remove(), 3000);
-            return false;
+            return "failedloading";
         }
 
         const html = await res.text();
         $(`<div class="${skillsClassname}"/>`).append($(html, _vdoc).find(".cdatal > span.marks.marki0:first").nextAll().andSelf()).appendTo($target);
 
-        return true;
+        return null;
     }
 
     let processingEvent = false;
@@ -70,8 +79,8 @@ await (async () => {
         if (processingEvent) return;
         processingEvent = true;
         try {
-            if (!toggleSelectedChara($(this))) return;
-            if (!await toggleSkillsOfSelectedChara($(this))) return;
+            if (toggleSelectedChara($(this))) return;
+            if (await toggleSkillsOfSelectedChara($(this))) return;
         }
         finally {
             processingEvent = false;
@@ -81,7 +90,7 @@ await (async () => {
         if (processingEvent) return;
         processingEvent = true;
         try {
-            if (!await toggleSkillsOfSelectedChara($(this))) return;
+            if (await toggleSkillsOfSelectedChara($(this))) return;
         }
         finally {
             processingEvent = false;
