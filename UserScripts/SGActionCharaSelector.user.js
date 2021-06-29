4cdb95542b1cc4d13de81170e7ac41e8f7b9e52f
pejuta <sp_dragon3125@hotmail.co.jp>
2021-06-29 21:14:53 +0900

    Update SGActionCharaSelector.user.js
    いらなそうな機能を一時的に無効にした。
    フォントサイズを調整した。

--- UserScripts/SGActionCharaSelector.user.js
+++ UserScripts/SGActionCharaSelector.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://twitter.com/11powder
 // @description Stroll Greenの各種行動画面のキャラ選択を便利にする
 // @include     http://st.x0.to/?mode=keizoku4*
-// @version     1.0.5
+// @version     1.0.6
 // @updateURL   https://pejuta.github.io/SGTools/UserScripts/SGActionCharaSelector.user.js
 // @downloadURL https://pejuta.github.io/SGTools/UserScripts/SGActionCharaSelector.user.js
 // @grant       none
@@ -85,16 +85,16 @@ await (async () => {
             $(e).nextUntil("hr.dashline").andSelf().wrapAll(`<span class="${singleSkillClassname}"/>`);
         });
 
-        $skills.find("span.marks.marki0+span").each((i, e) => {
-            const $type = $(e).children("span");
-            if ($type.is(".type,.type-1")) {
-                // セットなし or 基本スキル。花のマークなし
-                $type.html("&#x3000;");
-            } else {
-                const typeName = $type.children("span").html().substr(2, 2);
-                $type.html("&#x273f;").attr("title", typeName);
-            }
-        });
+        // $skills.find("span.marks.marki0+span").each((i, e) => {
+        //     const $type = $(e).children("span");
+        //     if ($type.is(".type,.type-1")) {
+        //         // セットなし or 基本スキル。花のマークなし
+        //         $type.html("&#x3000;");
+        //     } else {
+        //         const typeName = $type.children("span").html().substr(2, 2);
+        //         $type.html("&#x273f;").attr("title", typeName);
+        //     }
+        // });
 
         $skills.find("span.marks.marki0+span+span+small>b:empty").each((i, e) => {
             $(e).parent().next("br").next("small").children("span:nth-child(3)").css("color", "#704030;");
@@ -130,14 +130,14 @@ await (async () => {
 <style type="text/css">
     .block{display:block!important;}
     .inline{display:inline!important;}
-    .${skillsClassname}{font-size:8px;}
-    .${skillsClassname} span.marks.marki0{width:20px;}
-    .${skillsClassname} span.marks.marki0+span>span{display:inline-block;width:9px;}
-    .${skillsClassname} span.marks.marki0+span+span{width:120px!important;}
+    .${skillsClassname}{font-size:small;}
+    .${skillsClassname} span.marks.marki0{width:2em;}
+    .${skillsClassname} span.marks.marki0+span>span{display:inline-block;}
+    .${skillsClassname} span.marks.marki0+span+span{width:11em!important;}
     .${docClassname_HiddenEffects} .${skillsClassname} span.marks.marki0+span+span+small+br{display:none;}
     .${skillsClassname} span.marks.marki0+span+span+small+br+small{display:inline-block;}
     .${docClassname_HiddenEffects} .${skillsClassname} span.marks.marki0+span+span+small+br+small{display:none;}
-    .${skillsClassname} span.marks.marki0+span+span+small+br+small>span:first-child{padding-left:24px!important;}
+    .${skillsClassname} span.marks.marki0+span+span+small+br+small>span:first-child{padding-left:calc(2em + 4px)!important;}
     .${singleSkillClassname_NoCondition}{opacity:0.6;}
     .sgbutton{
         padding: 3px;
