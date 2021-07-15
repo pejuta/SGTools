20a988d42c79b547d1e3d051ab3d38d351d6b20c
pejuta <sp_dragon3125@hotmail.co.jp>
2021-07-15 14:39:29 +0900

    Update SGChatSearchTargetSelector.user.js
    影響はないけど微修正

--- UserScripts/SGChatSearchTargetSelector.user.js
+++ UserScripts/SGChatSearchTargetSelector.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://twitter.com/11powder
 // @description Stroll Greenのチャット検索対象タイムラインを選択可能にする
 // @include     /^http:\/\/st\.x0\.to\/?(?:\?mode=(?:chat|cdel)(?:&.*)?|index.php)?$/
-// @version     1.0.0
+// @version     1.0.1
 // @updateURL   https://pejuta.github.io/SGTools/UserScripts/SGChatSearchTargetSelector.user.js
 // @downloadURL https://pejuta.github.io/SGTools/UserScripts/SGChatSearchTargetSelector.user.js
 // @grant       none
@@ -14,7 +14,7 @@
     const TLLIST_TO_NAME = ["全て", "TL", "返信", "発言ツリー", "自分", "ユーザー", "周辺", "メッセージ", "リスト"];
 
     function replaceTLListInputWithSelect() {
-        const innerHtml = TLLIST_TO_NAME.map((e, i) => `<option value="${i}">${e}</option>`).join();
+        const innerHtml = TLLIST_TO_NAME.map((e, i) => `<option value="${i}">${e}</option>`).join("");
         const $select = $(`<select name="list">${innerHtml}</select>`);
         const $hiddenInput = $(".mainarea > .sheet:first form").children("[name='list']:first");
         $select.val($hiddenInput.val());
