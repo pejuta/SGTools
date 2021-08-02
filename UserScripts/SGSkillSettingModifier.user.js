a0abd4aeb08eb1522a18a4b979c5edbdca66fa5f
pejuta <sp_dragon3125@hotmail.co.jp>
2021-08-02 17:13:40 +0900

    Update SGSkillSettingModifier.user.js
    tab inに対応するためにfocusinイベントの実装をした

--- UserScripts/SGSkillSettingModifier.user.js
+++ UserScripts/SGSkillSettingModifier.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://twitter.com/11powder
 // @description Stroll Greenの戦闘設定を快適にする
 // @include     /^http:\/\/st\.x0\.to\/?(?:\?mode=keizoku1(&.*)?)?$/
-// @version     1.0.5.1
+// @version     1.0.5.2
 // @require     https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.14.0/Sortable.min.js
 // @updateURL   https://pejuta.github.io/SGTools/UserScripts/SGSkillSettingModifier.user.js
 // @downloadURL https://pejuta.github.io/SGTools/UserScripts/SGSkillSettingModifier.user.js
@@ -575,7 +575,8 @@
                 this.toggleSelect($(e.currentTarget).next());
             });
 
-            this.$vals.on("click", (e) => {
+            this.$vals.on("focusin", (e) => {
+                this.deactivateSelect($(".searchableselect_sel.active"));
                 this.activateSelect($(e.currentTarget).parent());
             }).on("keydown", (e) => {
                 if (e.keyCode === 13 /* enter */) {
