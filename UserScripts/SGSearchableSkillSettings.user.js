d4ca1ac44383e3642cd19b09b8bf5527e3c7042e
pejuta <sp_dragon3125@hotmail.co.jp>
2021-08-02 17:03:50 +0900

    Create SGSearchableSkillSettings.user.js

---
+++ UserScripts/SGSearchableSkillSettings.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        SGSearchableSkillSettings
+// @namespace   https://twitter.com/11powder
+// @description Stroll Greenのスキル名を検索可能にする
+// @include     /^http:\/\/st\.x0\.to\/?(?:\?mode=keizoku1(&.*)?)?$/
+// @version     1.0.4.3
+// @updateURL   https://pejuta.github.io/SGTools/UserScripts/SGSearchableSkillSettings.user.js
+// @downloadURL https://pejuta.github.io/SGTools/UserScripts/SGSearchableSkillSettings.user.js
+// @grant       none
+// ==/UserScript==
+
+(() => {
+    "use strict";
+
+})();
