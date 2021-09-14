f1ff89b7498c2f08b03f920ed06486b42e7f8f49
pejuta <sp_dragon3125@hotmail.co.jp>
2021-09-14 22:13:50 +0900

    Update SGFlowerDicLBMarker.user.js
    モード遷移機能の追加

--- UserScripts/SGFlowerDicLBMarker.user.js
+++ UserScripts/SGFlowerDicLBMarker.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        SGFlowerDicLBMarker
+// @name        SGFlowerDicEnhancer
 // @namespace   https://twitter.com/11powder
-// @description 簡易花図鑑で上限突破した花を簡単に確認できるようにする。
-// @match       http://st.x0.to/?mode=zukan2*
-// @version     1.0.0
+// @description 花図鑑の上限突破を簡単に確認できるようにしたり、モード変更を簡単にしたりする。
+// @match       http://st.x0.to/?mode=zukan*
+// @version     1.0.1
 // @updateURL   https://pejuta.github.io/SGTools/UserScripts/SGFlowerDicLBMarker.user.js
 // @downloadURL https://pejuta.github.io/SGTools/UserScripts/SGFlowerDicLBMarker.user.js
 // @grant       none
@@ -44,6 +44,10 @@
         }
     </style>`);
 
+    function alertResponseError(req) {
+        alert(res.statusText + " " + res.status);
+    }
+
     function getEnoFromLocation() {
         const m = /eno=(\d+)/.exec(location.search);
         if (!m) {
@@ -53,24 +57,87 @@
     }
 
     const _vdoc = document.implementation.createHTMLDocument();
-    async function applyLBStyleOnFlowers() {
-        const eno = getEnoFromLocation();
-
-        let targetUrl = "http://st.x0.to/?mode=zukan";
-        if (eno) {
-            targetUrl += "&zeno=" + eno;
+    async function loadOtherDicModeFrame() {
+        if ($(".profile ~ .framearea:hidden").length > 0) {
+            return true;
         }
 
+        const $switchDicA = $(".profile ~ .framearea").children("p:first-of-type").children("a:first-of-type");
+        const targetUrl = $switchDicA.attr("href");
         const res = await fetch(targetUrl);
         if (!res.ok) {
-            alert(res.statusText + " " + res.status);
-            return;
+            alertResponseError(res);
+            return false;
         }
         const html = await res.text();
+        $(html, _vdoc).find(".profile ~ .framearea").hide().insertAfter($(".profile ~ .framearea"));
+
+        appendSimpleDicButton();
+
+        $(".profile ~ .framearea").children("p:first-of-type").children("a:first-of-type").on("click", (e) => {
+            e.preventDefault();
+            switchDicModes(e.currentTarget);
+        });
+
+        return true;
+    }
+
+    async function loadThenSwitchDicModes() {
+        const $switchDicA = $(".profile ~ .framearea").children("p:first-of-type").children("a:first-of-type");
+        if (!await loadOtherDicModeFrame()) {
+            return false;
+        }
+        await switchDicModes($switchDicA[0]);
+
+        return true;
+    }
+
+    async function switchDicModes(anch) {
+        history.pushState("", "", $(anch).attr("href"));
+        await fadeSwitchFrames();
+    }
+
+    function fadeSwitchFrames() {
+        return new Promise((resolve) => {
+            const $hidden = $(".profile ~ .framearea:hidden");
+            const $visible = $(".profile ~ .framearea:visible");
+            $visible.fadeOut(200, () => $hidden.fadeIn(200, () => resolve()));
+        });
+    }
+
+    function appendSimpleDicButton() {
+        if ($("#showlbbtn").length > 0) {
+            return;
+        }
+        const $lastBrToInsertBefore = $(".profile ~ .framearea > .frameareab").prev("p").children("br:last");
+        if ($lastBrToInsertBefore.length === 0) {
+            return;
+        }
+        $("<span id='showlbbtn'>上限突破の表示切り替え</span>").insertBefore($lastBrToInsertBefore).on("click", async () => {
+            await loadOtherDicModeFrame();
+            toggleLBStyleOnFlowers();
+        });
+    }
+
+    function extractDetailedFlowerNum(flower) {
+        return parseInt($(flower).children("b:first-of-type").html().split("　")[0].slice(4), 10);
+    }
+
+    function toggleLBStyleOnFlowers() {
+        const $markedFrames = $(".charaframe2.flowerlb");
+        if ($markedFrames.length > 0) {
+            $markedFrames.removeClass("flowerlb");
+            return;
+        }
+
+        const $simpleFlowers = $(".profile ~ .framearea > .talkarea");
+        if ($simpleFlowers.length === 0) {
+            return;
+        }
 
         const lbSet = new Set();
-        $(html, _vdoc).find(".profile + .framearea > .talkarea").each((i, e) => {
-            const num = parseInt($(e).children("b:first").html().split("　")[0].slice(4), 10);
+        $simpleFlowers.each((i, e) => {
+            const num = extractDetailedFlowerNum(e);
             const isLB = $(e).children("small:last").children("b").filter((i, b) => {
                 return $(b).attr("style") === "color: #30aa10;";
             }).length > 0;
@@ -79,12 +146,46 @@
             }
         });
 
-        $(".profile + .framearea > .frameareab > div > .charaframe2").each((i, e) => {
+        $(".profile ~ .framearea > .frameareab > div > .charaframe2").each((i, e) => {
             if (lbSet.has(i + 1)) {
                 e.classList.add("flowerlb");
             }
         });
     }
 
-    $("<span id='showlbbtn'>上限突破の表示</span>").insertBefore($(".profile + .framearea > .frameareab").prev("p").children("br:last")).one("click", () => applyLBStyleOnFlowers());
+    appendSimpleDicButton();
+
+    $(".profile ~ .framearea").children("p:first-of-type").children("a:first-of-type").one("click", async (e) => {
+        e.preventDefault();
+        await loadThenSwitchDicModes();
+    });
+
+    $(document).on("click", ".profile ~ .framearea > .talkarea > img", async (evt) => {
+        const $thisFlower = $(evt.currentTarget).parent(".talkarea");
+        const num = extractDetailedFlowerNum($thisFlower);
+        if (!await loadThenSwitchDicModes()) {
+            return;
+        }
+        const $simple = $(".profile ~ .framearea > .frameareab > div > .charaframe2");
+        $simple.eq(num - 1)[0].scrollIntoView(false);
+    });
+    $(document).on("click", ".profile ~ .framearea > .frameareab > div > .charaframe2 > img", async (evt) => {
+        const $thisFlower = $(evt.currentTarget).parent(".charaframe2");
+        if (evt.currentTarget.style.opacity) {
+            // ????
+            return;
+        }
+        const $simple = $(".profile ~ .framearea > .frameareab > div > .charaframe2");
+        const index = $simple.index($thisFlower);
+        if (!await loadThenSwitchDicModes()) {
+            return;
+        }
+        const $targetDetailed = $(".profile ~ .framearea > .talkarea").filter((i, e) => {
+            const num = extractDetailedFlowerNum(e);
+            return index === num - 1;
+        });
+        if ($targetDetailed.length > 0) {
+            $targetDetailed[0].scrollIntoView(false);
+        }
+    });
 })();
